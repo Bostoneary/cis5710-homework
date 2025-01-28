a16059bcb0ce92e973f2361eb8b0e7765c5fb66c
Zhiye Zhang <bubucse@foxmail.com>
2025-01-28 08:07:50 -0500

    finish hw2a

--- hw2a-divider/divider_unsigned.sv
+++ hw2a-divider/divider_unsigned.sv
@@ -14,6 +14,29 @@ module divider_unsigned (
 );
 
     // TODO: your code here
+    wire [31:0] remainder_tmp[33];
+    wire [31:0] dividend_tmp[33];
+    wire [31:0] quotient_tmp[33];
+
+    assign remainder_tmp[0]=0;
+    assign dividend_tmp[0]=i_dividend;
+    assign quotient_tmp[0]=0;
+
+    genvar i;
+    for(i=0;i<32;i++)
+    begin
+        divu_1iter d(
+            .i_dividend(dividend_tmp[i]),
+            .i_divisor(i_divisor),
+            .i_remainder(remainder_tmp[i]),
+            .i_quotient(quotient_tmp[i]),
+            .o_dividend(dividend_tmp[i+1]),
+            .o_remainder(remainder_tmp[i+1]),
+            .o_quotient(quotient_tmp[i+1])
+        );
+    end
+    assign o_remainder=remainder_tmp[32];
+    assign o_quotient=quotient_tmp[32];
 
 endmodule
 
@@ -39,12 +62,26 @@ module divu_1iter (
         dividend = dividend << 1;
     }
     */
-    logic [31:0] remainder_cmp;
 
     // TODO: your code here
-    assign remainder_cmp=(i_remainder<<1)|((i_dividend>>31)&32'b1);
-    assign o_remainder=(remainder_cmp>=i_divisor)?(remainder_cmp-i_divisor):remainder_cmp;
-    assign o_quotient=(remainder_cmp>=i_divisor)?((i_quotient<<1)&32'b1):(i_quotient<<1);
-    assign o_dividend=i_dividend<<1;
-
+    logic [31:0] remainder_tmp;
+    logic [31:0] quotient_tmp;
+    logic [31:0] dividend_tmp;
+    always_comb
+    begin
+        remainder_tmp=(i_remainder<<1)|((i_dividend>>31)&32'b1);
+        if(remainder_tmp<i_divisor)
+        begin
+            quotient_tmp=i_quotient<<1;
+        end
+        else
+        begin
+            remainder_tmp=remainder_tmp-i_divisor;
+            quotient_tmp=(i_quotient<<1)|32'b1;
+        end
+        dividend_tmp=i_dividend<<1;
+    end
+    assign o_dividend=dividend_tmp;
+    assign o_quotient=quotient_tmp;
+    assign o_remainder=remainder_tmp;
 endmodule
