dd2c09717442ad2cfe3685fde50f5477f2dc860e
yunlong4 <yunlong4@seas.upenn.edu>
2025-02-09 13:31:59 -0500

    1

--- hw3-singlecycle/DatapathSingleCycle.sv
+++ hw3-singlecycle/DatapathSingleCycle.sv
@@ -478,7 +478,7 @@ module DatapathSingleCycle (
           end
           else
           begin
-            pcNext=pcCurrent+4;
+            pcNext=pcCurrent+3;
           end
         end
         else if(insn_blt)
